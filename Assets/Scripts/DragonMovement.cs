703a7dec376c0cc1b68e73fdba460781161159f5
BluShine <BluShine@users.noreply.github.com>
2020-02-02 17:48:54 -0600

    Dragon animates

--- Assets/Scripts/DragonMovement.cs
+++ Assets/Scripts/DragonMovement.cs
@@ -9,7 +9,19 @@ public class DragonMovement : MonoBehaviour
 
     Rigidbody body;
 
-    
+    //flapping
+    public Animator flapping;
+    float fVel = 0;
+    public float flappingSpeedBoost = 2;
+
+    //Tilting
+    public Transform tiltForward;
+    public Transform tiltLeft;
+    public Transform tiltRight;
+    public Transform dragonMesh;
+    Vector2 smoothInput = Vector2.zero;
+    Vector2 smoothInputVel = Vector2.zero;
+    Vector3 inputVector = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +30,44 @@ public class DragonMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        //tilting
+        smoothInput = Vector2.SmoothDamp(smoothInput, new Vector2(inputVector.x, inputVector.z), ref smoothInputVel, .2f, 10, Time.fixedDeltaTime);
+        Quaternion rotation = Quaternion.identity;
+        if(smoothInput.x < 0)
+        {
+            rotation = Quaternion.Lerp(rotation, tiltLeft.rotation, Mathf.Abs(smoothInput.x));
+        } else
+        {
+            rotation = Quaternion.Lerp(rotation, tiltRight.rotation, Mathf.Abs(smoothInput.x));
+        }
+        if (smoothInput.y > 0)
+        {
+            rotation = Quaternion.Lerp(rotation, tiltForward.rotation, Mathf.Abs(smoothInput.y));
+        }
+        else
+        {
+            rotation = Quaternion.Lerp(rotation, Quaternion.Euler(-tiltForward.rotation.eulerAngles), Mathf.Abs(smoothInput.y));
+        }
+        dragonMesh.transform.rotation = rotation;
+    }
+
     void FixedUpdate()
     {
-        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //input
+        inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if (inputVector.magnitude > 1)
         {
             inputVector.Normalize();
         }
+
+        //flapping
+        flapping.speed = Mathf.SmoothDamp(flapping.speed, 
+            1 + flappingSpeedBoost * inputVector.magnitude, 
+            ref fVel, .2f, 10, Time.fixedDeltaTime);
+
+        //movement
         if (inputVector.magnitude == 0)
         {
             if(body.velocity.magnitude < Time.fixedDeltaTime * accel)
